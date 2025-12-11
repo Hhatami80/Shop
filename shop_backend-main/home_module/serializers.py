69d3a6b7689de3ba376c86b28b4477db13d4073d
aseifi880 <aseifi880@gmail.com>
2025-12-11 13:22:23 +0330

    add dynamic price for bestseller serializer

--- shop_backend-main/home_module/serializers.py
+++ shop_backend-main/home_module/serializers.py
@@ -13,7 +13,14 @@ class ProductMainPageSerializer(serializers.ModelSerializer):
 
 class BestSellerSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(use_url=True)
+    price = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
-        fields = ['id', 'title', 'price', 'description', 'image']
+        fields = ['id', 'title', 'price', 'description', 'image', 'is_purchasable']
+
+    def get_price(self, obj):
+        if obj.is_purchasable:
+            return obj.price
+        else:
+            return "برای سفارش تماس بگیرید."
